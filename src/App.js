1c56a8304333bc8e360d5626903789f12978d509
Fotis Tsakiris <footios76@gmail.com>
2019-03-11 11:22:15 +0200

    Initial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 import Persons from './containers/Persons';
 
 class App extends Component {
@@ -7,7 +8,8 @@ class App extends Component {
     return (
       <div className="App">
         <ol>
-          <li>Turn this app into one which does NOT use local state (in components) but instead uses Redux</li>
+          <li>Turn this app into one which 
+            does NOT use local state (in components) but instead uses Redux</li>
         </ol>
         <Persons />
       </div>
@@ -17,32 +19,3 @@ class App extends Component {
 
 export default App;
 
-
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <p>
-//             Edit <code>src/App.js</code> and save to reload.
-//           </p>
-//           <a
-//             className="App-link"
-//             href="https://reactjs.org"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             Learn React
-//           </a>
-//         </header>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
