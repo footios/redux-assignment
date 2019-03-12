56630f321ae9ddbacd85b084e50c11b612518163
Fotis Tsakiris <footios76@gmail.com>
2019-03-12 11:39:24 +0200

    use local state through redux

--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -1,91 +1,93 @@
 import React, { Component } from 'react';
 
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 
 import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
-import * as actionTypes from '../store/actions'
-
+import * as actionTypes from '../store/actions';
 
 class Persons extends Component {
-    // state = {
-    //     persons: []
-    // }
+	state = {
+		name: '',
+		age: ''
+	};
 
-    // personAddedHandler = () => {
-    //     const newPerson = {
-    //         id: Math.random(), // not really unique but good enough here!
-    //         name: 'Max',
-    //         age: Math.floor( Math.random() * 40 )
-    //     }
-    //     this.setState( ( prevState ) => {
-    //         return { persons: prevState.persons.concat(newPerson)}
-    //     } );
+	nameHandler = (e) => {
+		this.setState({
+			name: e.target.value
+		});
+	};
 
-    // }
+	ageHandler = (e) => {
+		this.setState({
+			age: e.target.value
+		});
+	};
 
-    // personDeletedHandler = (personId) => {
-    //     this.setState( ( prevState ) => {
-    //         return { persons: prevState.persons.filter(person => person.id !== personId)}
-    //     } );
-    // }
+	render() {
+		return (
+			<div style={{ textAlign: 'center' }}>
+				<span>Persons added: {this.props.ctr}</span>
+				<br />
+				<br />
+				<div style={{ textAlign: 'center' }}> Additions by date</div>
+				<button onClick={() => this.props.onAddition(this.props.ctr)}>Additions</button>
+				<br />
+				<ul>
+					{this.props.adds.map((addition) => (
+						<li key={addition.date}>
+							{addition.date} : Persons added: {addition.persons}
+						</li>
+					))}
+				</ul>
 
-    render () {
-        return (
-            <div>
-                <div style={{textAlign:'center'}} >Persons added: {this.props.ctr}</div>
-                <br></br>
-                <div style={{textAlign:'center'}}> Additions by date 
-                <br></br>
-                <button style={{width:'10%'}} onClick={() => this.props.onAddition(this.props.ctr)}>
-                Add by date </button>
-                <br></br>
-                <ul>
-                {this.props.adds.map(addition => (
-                    <li key={addition.date}>{addition.date} : Persons added: {addition.persons}</li>
-                ))} 
-                </ul>
-            </div>
-                
-                
-                <AddPerson personAdded={this.props.onAddPerson} />
-                {this.props.pers.map(person => (
-                    <Person 
-                        key={person.id}
-                        name={person.name} 
-                        age={person.age} 
-                        clicked={() => this.props.onDeletePerson(person.id)}/>
-                ))}
-            </div>
-        );
-    }
+				<input type="text" placeholder="name" value={this.state.name} onChange={this.nameHandler} />
+				<input type="number" placeholder="age" value={this.state.age} onChange={this.ageHandler} />
+
+				<AddPerson personAdded={() => this.props.onAddPerson(this.state.name, this.state.age)} />
+				{this.props.pers.map((person) => (
+					<Person
+						key={person.id}
+						name={person.name}
+						age={person.age}
+						clicked={() => this.props.onDeletePerson(person.id)}
+					/>
+				))}
+			</div>
+		);
+	}
 }
 
-const mapStateToProps = state => {
-    return {
-        ctr: state.addPer.counter,
-        pers: state.addPer.persons,
-        adds: state.byD.additions
-    }
-}
+const mapStateToProps = (state) => {
+	return {
+		ctr: state.addPer.counter,
+		pers: state.addPer.persons,
+		adds: state.byD.additions
+	};
+};
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onAddPerson: () => dispatch({
-            type: actionTypes.ADD_PERSON,
-            newPerson: {
-                id: Math.random(), // not really unique but good enough here!
-                name: 'Max',
-                age: Math.floor( Math.random() * 40 )
-            }
-        }),
-        onDeletePerson: (id) => dispatch({
-            type: actionTypes.DELETE_PERSON, personId: id
-        }),
-        onAddition: (persons) => dispatch({
-            type: actionTypes.ADDITION, personsAdded: persons
-        })
-    }
-}
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onAddPerson: (name, age) =>
+			dispatch({
+				type: actionTypes.ADD_PERSON,
+				newPerson: {
+					id: Math.random(), // not really unique but good enough here!
+					name: name,
+					age: age
+				}
+			}),
+		onDeletePerson: (id) =>
+			dispatch({
+				type: actionTypes.DELETE_PERSON,
+				personId: id
+			}),
+		onAddition: (persons) =>
+			dispatch({
+				type: actionTypes.ADDITION,
+				personsAdded: persons
+			})
+	};
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Persons);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,35 +1,35 @@
-import * as actionTypes from './actions'
+// import * as actionTypes from './actions'
 
-const initialState = {
-    counter: 0,
-    persons: [],
-    additions: []
-}
+// const initialState = {
+//     counter: 0,
+//     persons: [],
+//     additions: []
+// }
 
-const reducer = (state = initialState, action) => {
-    console.log(state.additions);
+// const reducer = (state = initialState, action) => {
+//     console.log(state.additions);
     
-    switch (action.type) {
-        case actionTypes.ADD_PERSON:
-            return {
-                ...state,
-                counter: state.counter + 1,
-                persons: state.persons.concat(action.newPerson)
-            }
-            case actionTypes.DELETE_PERSON:
-            return {
-                ...state,
-                counter: state.counter - 1,
-                persons: state.persons.filter(person => person.id !== action.personId)
-            }
-            case actionTypes.ADDITION:
-            return {
-                ...state,
-                additions: state.additions.concat( {date: new Date().toString(), persons: action.personsAdded})
-            }
-        default:
-            return state
-    }
-}
+//     switch (action.type) {
+//         case actionTypes.ADD_PERSON:
+//             return {
+//                 ...state,
+//                 counter: state.counter + 1,
+//                 persons: state.persons.concat(action.newPerson)
+//             }
+//             case actionTypes.DELETE_PERSON:
+//             return {
+//                 ...state,
+//                 counter: state.counter - 1,
+//                 persons: state.persons.filter(person => person.id !== action.personId)
+//             }
+//             case actionTypes.ADDITION:
+//             return {
+//                 ...state,
+//                 additions: state.additions.concat( {date: new Date().toString(), persons: action.personsAdded})
+//             }
+//         default:
+//             return state
+//     }
+// }
 
-export default reducer
+// export default reducer
